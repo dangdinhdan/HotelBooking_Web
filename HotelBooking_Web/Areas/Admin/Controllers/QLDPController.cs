95f809000b0237b0a12dab5f529f6c2cd12759e2
dangdinhdan <dangdan2132@gmail.com>
2025-11-12 23:44:30 +0700

    loi

---
+++ HotelBooking_Web/Areas/Admin/Controllers/QLDPController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelBooking_Web.Areas.Admin.Controllers
+{
+    public class QLDPController : Controller
+    {
+        // GET: Admin/QLDP
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CheckIn()
+        {
+            return View();
+        }
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
